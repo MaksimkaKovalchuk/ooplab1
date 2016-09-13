640e8afe6ed1adb86fc92ce62fd8fee5b4a3e1a9
Maksim <maksimkakovalchuk1203@gmail.com>
2016-09-13 17:40:32 +0300

    start project

---
+++ OOP_lab01/OOP_lab01.cpp
@@ -0,0 +1,23 @@
+// OOP_lab01.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+// начало объ€влени€ класса
+class CppStudio // им€ класса
+{
+public: // спецификатор доступа
+	void message() // функци€ (метод класса) вывод€ща€ сообщение на экран
+	{
+		cout << "website: cppstudio.com\ntheme: Classes and Objects in C + +\n";
+	}
+}; // конец объ€влени€ класса CppStudio
+
+int main(int argc, char* argv[])
+{
+	CppStudio objMessage; // объ€вление объекта
+	objMessage.message(); // вызов функции класса message
+	system("pause");
+	return 0;
+}
